2720d079ad0151bc08d7fe8a4c5ba3faff657d1b
Javier Rodriguez <javrodri@student.42madrid.com>
2021-02-04 19:14:03 +0100

    update

--- ruby02/ex01/isneg.rb
+++ ruby02/ex01/isneg.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 num = gets.chomp.to_i
-
 if num > 0
     puts "Este numero es positivo"
 elsif num == 0
--- ruby02/ex03/mult.rb
+++ ruby02/ex03/mult.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
 puts "Introduzca un primer número:"
-n1 = gets.chomp.to_i
+num1 = gets.chomp.to_i
 puts "Introduzca un segundo número:"
-n2 = gets.chomp.to_i
-n3 = n1 * n2
-if n3 < 0
+num2 = gets.chomp.to_i
+num3 = num1 * num2
+if num3 < 0
 	puts "El resultado es negativo"
-elsif n3 > 0
+elsif num3 > 0
 	puts "El resultado es positivo"
 else
 	puts "El resultado es positivo y negativo"
 end
-puts n1.to_s + " * " + n2.to_s + " = " + n3.to_s
+puts num1.to_s + " X " + num2.to_s + " = " + num3.to_s
