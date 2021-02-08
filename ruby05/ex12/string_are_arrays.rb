5ec2dba6138832e13fde78fd768c987611ad6d27
Javier <jrg1991@gmail.com>
2021-02-09 00:12:31 +0100

    rb05-ex12

--- ruby05/ex12/string_are_arrays.rb
+++ ruby05/ex12/string_are_arrays.rb
@@ -1,1 +1,22 @@
 #!/usr/bin/env ruby
+if ARGV.size != 1
+    puts "none"
+else
+    i = 0
+    str = ARGV[0]
+    str = str.split("")
+    str.each do |s|
+        if s == "z" || s == "Z"
+            i += 1
+        end
+    end
+    if i == 0
+        puts "none"
+    else
+        while i > 0
+            print "z"
+            i -= 1
+        end
+        puts
+    end
+end
