3e772b293cc2c3b56c511d8f60b83121ec031861
Javier <jrg1991@gmail.com>
2021-02-09 00:15:23 +0100

    rb05-ex13

--- ruby05/ex13/append_it.rb
+++ ruby05/ex13/append_it.rb
@@ -1,1 +1,14 @@
 #!/usr/bin/env ruby
+if !ARGV[0]
+    puts "none"
+else
+    ARGV.each do |s|
+        if s.match("ismo")
+            puts s
+        else
+            s = s[0...-1]
+            s = s + "ismo"
+            puts s
+        end
+    end
+end
