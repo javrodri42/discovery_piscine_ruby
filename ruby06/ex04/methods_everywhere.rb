08ccaf39adfdef6ac5935eba459f946ca2553e37
Javier Rodriguez <javrodri@student.42madrid.com>
2021-02-09 16:44:22 +0100

    ex05

---
+++ ruby06/ex04/methods_everywhere.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+def reduce(word)
+    puts word[0, 8]
+end
+
+def agranda(word)
+    str = word.dup
+    while str.size < 8
+        str << "z"
+    end
+    puts str
+
+end
+
+if ARGV.size < 1
+    puts "none"
+else
+    ARGV.each do |n|
+        if n.size < 8
+            agranda(n)
+        elsif n.size > 8
+            reduce(n)
+        else
+            puts n
+        end
+end
+end
